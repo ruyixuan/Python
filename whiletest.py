f400e339887e56a90737e5358877199e73ed2f2f
ruyixuan <ruyixuan@hotmail.com>
2018-01-04 17:28:40 +0800

    no message

---
+++ whiletest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+var = 0
+while var < 10:
+	num = input(str(var)+"Enter a number:")
+	var += 1
+	if num == "fuck":
+		print ("不许耍流氓")
+		break
+else:
+	print ("超过限制！")
